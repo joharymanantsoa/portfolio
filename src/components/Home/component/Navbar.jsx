7f06633095978ace2d7fb9e4aaa51edf74a32a4d
Parlss29 <aina.razafinjato29@gmail.com>
2025-06-10 14:38:19 +0300

    chore: refactor Navbar component for better readability

--- src/components/Home/component/Navbar.jsx
+++ src/components/Home/component/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
     <nav className="fixed top-0 left-0 w-full bg-black  shadow z-50">
       <div className="max-w-7xl mx-auto px-4 py-3 flex items-center justify-between">
         <div className="flex items-center space-x-4">
-          <img src="../../../../public/profil-logo.png" alt="profil-logo" className="h-12 w-12" />
+          <img src="profil-logo.png" alt="profil-logo" className="h-12 w-12" />
           {/* <div className="text-xl font-bold text-red-600">Sarobidy</div> */}
         </div>
 
