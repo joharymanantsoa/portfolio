82b1a782a1273f82178665058f9c1273718ae515
Parlss29 <aina.razafinjato29@gmail.com>
2025-06-10 14:53:42 +0300

    chore: final commits
    - improve code formatting and enhance visual effects in Formation and Header components

--- src/components/Formation/Formation.jsx
+++ src/components/Formation/Formation.jsx
@@ -16,13 +16,13 @@ function Formation() {
   const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth >= 1434);
   const btnRef = useRef(null);
   const btnRef1 = useRef(null);
-  const [isCertification,setIsCertification] = useState(true);
+  const [isCertification, setIsCertification] = useState(true);
 
   useEffect(() => {
     // Initial style for buttons
     btnRef.current.className = styles.btnRef;
     btnRef1.current.className = styles.btnRef1;
-  },[]);
+  }, []);
 
   useEffect(() => {
     const handleResize = () => {
@@ -65,118 +65,131 @@ function Formation() {
       </center>
       <div className="flex lg:flex-row md:flex-col flex-col items-center sm:justify-center justify-center">
         <div className="w-full sm:w-[350px] md:w-[350px] lg:w-[390px] flex-col sm:justify-center lg:justify-end justify-end min-[1024px]:mr-[-50px] align-baseline">
-            {isCertification ? (<>
-          <div className="flex-1 bg-[#F91B1B70] m-[20px] px-[20px] py-[10px] sm:w-[300px] md:w-[300px] lg:w-[320px] max-w-[320px] hover:cursor-pointer hover:shadow-[#F91B1B] hover:shadow-sm">
-            <div className="flex">
-              <h5 className="text-[18px] text-[#F91B1B]">Certificated Cybersecurity Analyst | C3SAI</h5>
-              <img className="w-15 h-15" src={c3sai} alt="c3sai" />
+          {isCertification ? (<>
+            <div className="flex-1 bg-[#F91B1B70] m-[20px] px-[20px] py-[10px] sm:w-[300px] md:w-[300px] lg:w-[320px] max-w-[320px] hover:cursor-pointer hover:shadow-[#F91B1B] hover:shadow-sm">
+              <div className="flex">
+                <h5 className="text-[18px] text-[#F91B1B]">Certificated Cybersecurity Analyst | C3SAI</h5>
+                <img className="w-15 h-15" src={c3sai} alt="c3sai" />
+              </div>
+              <p>Cyber Warfare Labs</p><br></br>
+              <div className="flex justify-between w-full">
+                <span className="text-[#F91B1B] whitespace-nowrap">Cambridge</span>
+                <span className="whitespace-nowrap">Octobre 2023</span>
+              </div>
             </div>
-            <p>Cyber Warfare Labs</p><br></br>
-            <div className="flex justify-between w-full">
-              <span className="text-[#F91B1B] whitespace-nowrap">Cambridge</span>
-              <span className="whitespace-nowrap">Octobre 2023</span>
+            <div className={clsx(
+              'flex-1 bg-[#F91B1B70] m-[20px] px-[20px] py-[10px] sm:w-[300px] md:w-[300px] lg:w-[320px] max-w-[320px] hover:cursor-pointer hover:shadow-[#F91B1B] hover:shadow-sm',
+              {
+                'ml-[-50px]': isLargeScreen,
+              }
+            )}>
+              <div className="flex">
+                <h5 className="text-[18px] text-[#F91B1B]">YALI REGIONAL LEADERSHIP CENTER</h5>
+                <img className="w-15 h-15" src={yali} alt="yali" />
+              </div>
+              <p>YALI RLC & USAID</p><br></br>
+              <div className="flex justify-between w-full">
+                <span className="text-[#F91B1B] whitespace-nowrap">Afrique de Sud</span>
+                <span className="whitespace-nowrap">Août 2023</span>
+              </div>
             </div>
-          </div>
-          <div className={clsx(
-            'flex-1 bg-[#F91B1B70] m-[20px] px-[20px] py-[10px] sm:w-[300px] md:w-[300px] lg:w-[320px] max-w-[320px] hover:cursor-pointer hover:shadow-[#F91B1B] hover:shadow-sm',
-            {
-              'ml-[-50px]': isLargeScreen,
-            }
-          )}>
-            <div className="flex">
-              <h5 className="text-[18px] text-[#F91B1B]">YALI REGIONAL LEADERSHIP CENTER</h5>
-              <img className="w-15 h-15" src={yali} alt="yali" />
+            <div className="flex-1 bg-[#F91B1B70] m-[20px] px-[20px] py-[10px] sm:w-[300px] md:w-[300px] lg:w-[320px] max-w-[320px] hover:cursor-pointer hover:shadow-[#F91B1B] hover:shadow-sm">
+              <div className="flex">
+                <h5 className="text-[18px] text-[#F91B1B]">Fortinet Certified Fundamentals in Cybersecurity</h5>
+                <img className="w-15 h-15" src={fortinet} alt="fortinet" />
+              </div>
+              <p>Fortinet</p><br></br>
+              <div className="flex justify-between w-full">
+                <span className="text-[#F91B1B] whitespace-nowrap">Sunnyvale</span>
+                <span className="whitespace-nowrap">Avril 2023</span>
+              </div>
             </div>
-            <p>YALI RLC & USAID</p><br></br>
-            <div className="flex justify-between w-full">
-              <span className="text-[#F91B1B] whitespace-nowrap">Afrique de Sud</span>
-              <span className="whitespace-nowrap">Août 2023</span>
+          </>) : (<>
+            <div className="flex-1 bg-[#F91B1B70] m-[20px] px-[20px] py-[10px] sm:w-[300px] md:w-[300px] lg:w-[320px] max-w-[320px] hover:cursor-pointer hover:shadow-[#F91B1B] hover:shadow-sm">
+              <div className="flex">
+                <h5 className="text-[18px] text-[#F91B1B]">ESIEE-IT Paris / ESTI Antanimena</h5>
+                <img className="w-15 h-15" src={esiee} alt="esiee" />
+              </div>
+              <p>MASTER en Infrastructure et Cybersécurité</p><br></br>
+              <div className="flex justify-between w-full">
+                <span className="text-[#F91B1B] whitespace-nowrap">Madagascar</span>
+                <span className="whitespace-nowrap">2021 - 2024</span>
+              </div>
             </div>
-          </div>
-          <div className="flex-1 bg-[#F91B1B70] m-[20px] px-[20px] py-[10px] sm:w-[300px] md:w-[300px] lg:w-[320px] max-w-[320px] hover:cursor-pointer hover:shadow-[#F91B1B] hover:shadow-sm">
-            <div className="flex">
-              <h5 className="text-[18px] text-[#F91B1B]">Fortinet Certified Fundamentals in Cybersecurity</h5>
-              <img className="w-15 h-15" src={fortinet} alt="fortinet" />
-            </div>
-            <p>Fortinet</p><br></br>
-            <div className="flex justify-between w-full">
-              <span className="text-[#F91B1B] whitespace-nowrap">Sunnyvale</span>
-              <span className="whitespace-nowrap">Avril 2023</span>
-            </div>
-          </div>
-          </>):(<>
-          <div className="flex-1 bg-[#F91B1B70] m-[20px] px-[20px] py-[10px] sm:w-[300px] md:w-[300px] lg:w-[320px] max-w-[320px] hover:cursor-pointer hover:shadow-[#F91B1B] hover:shadow-sm">
-            <div className="flex">
-              <h5 className="text-[18px] text-[#F91B1B]">ESIEE-IT Paris / ESTI Antanimena</h5>
-              <img className="w-15 h-15" src={esiee} alt="esiee" />
-            </div>
-            <p>MASTER en Infrastructure et Cybersécurité</p><br></br>
-            <div className="flex justify-between w-full">
-              <span className="text-[#F91B1B] whitespace-nowrap">Madagascar</span>
-              <span className="whitespace-nowrap">2021 - 2024</span>
-            </div>
-          </div>
           </>)}
         </div>
-        <div className="flex-1 w-[476px] h-[398px] mx-auto object-cover" id={styles.image}>
-          <img
-            src={monImage}
-            alt="sarobidy-profile"
-            className="w-[476px] h-[398px] mx-auto flex object-cover"
-          />
+        <div className="flex-1"  id={styles.image}>
+          <div className="relative w-full h-full flex justify-center items-center py-8">
+            {/* Effet de glow amélioré et élargi */}
+            <div className="absolute bg-red-900/70 w-[340px] h-[340px] rounded-full blur-3xl"></div>
+
+            {/* Container d'image circulaire */}
+            <div className="relative z-10 w-[476px] h-[398px] rounded-full overflow-hidden">
+              <img
+                src={monImage}
+                alt="Sarobidy ANDRIAMIALY"
+                className="w-[476px] h-[398px] object-cover object-top" // object-top pour focus sur le visage
+                style={{ objectPosition: "center 30%" }} // Ajustement fin de la position verticale
+                loading="eager"
+              />
+            </div>
+          </div>
         </div>
+
+
+
         <div className="w-full sm:w-[350px] md:w-[350px] lg:w-[390px] flex-col justify-end align-baseline">
-        {isCertification ? (<>
-          <div className="flex-1 bg-[#f91b1b70] m-[20px] px-[20px] py-[10px] sm:w-[300px] max-[1023px]:mt-0 md:w-[300px] lg:w-[320px] max-w-[320px] hover:cursor-pointer hover:shadow-[#F91B1B] hover:shadow-sm">
-            <div className="flex">
-              <h5 className="text-[18px] text-[#F91B1B] pr-6">API Penetration Testing</h5>
-              <img className="w-15 h-15" src={apisec} alt="apisec" />
+          {isCertification ? (<>
+            <div className="flex-1 bg-[#f91b1b70] m-[20px] px-[20px] py-[10px] sm:w-[300px] max-[1023px]:mt-0 md:w-[300px] lg:w-[320px] max-w-[320px] hover:cursor-pointer hover:shadow-[#F91B1B] hover:shadow-sm">
+              <div className="flex">
+                <h5 className="text-[18px] text-[#F91B1B] pr-6">API Penetration Testing</h5>
+                <img className="w-15 h-15" src={apisec} alt="apisec" />
+              </div>
+              <p>APISEC University</p><br></br>
+              <div className="flex justify-between w-full">
+                <span className="text-[#F91B1B] whitespace-nowrap">San Francisco</span>
+                <span className="whitespace-nowrap">Avril 2023</span>
+              </div>
             </div>
-            <p>APISEC University</p><br></br>
-            <div className="flex justify-between w-full">
-              <span className="text-[#F91B1B] whitespace-nowrap">San Francisco</span>
-              <span className="whitespace-nowrap">Avril 2023</span>
+            <div className={clsx(
+              'flex-1 bg-[#F91B1B70] m-[20px] px-[20px] py-[10px] sm:w-[300px] md:w-[300px] lg:w-[320px] max-w-[320px] hover:cursor-pointer hover:shadow-[#F91B1B] hover:shadow-sm',
+              {
+                'ml-[90px]': isLargeScreen,
+              }
+            )}>
+              <div className="flex">
+                <h5 className="text-[18px] text-[#F91B1B]">Certificate of Superior Level English C2</h5>
+                <img className="w-15 h-15" src={itti} alt="itti" />
+              </div>
+              <p>International Testing &Training institute</p><br></br>
+              <div className="flex justify-between w-full">
+                <span className="text-[#F91B1B] whitespace-nowrap">New York</span>
+                <span className="whitespace-nowrap">Août 2022</span>
+              </div>
             </div>
-          </div>
-          <div className={clsx(
-            'flex-1 bg-[#F91B1B70] m-[20px] px-[20px] py-[10px] sm:w-[300px] md:w-[300px] lg:w-[320px] max-w-[320px] hover:cursor-pointer hover:shadow-[#F91B1B] hover:shadow-sm',
-            {
-              'ml-[90px]':isLargeScreen,
-            }
-          )}>
-            <div className="flex">
-              <h5 className="text-[18px] text-[#F91B1B]">Certificate of Superior Level English C2</h5>
-              <img className="w-15 h-15" src={itti} alt="itti" />
-            </div>
-            <p>International Testing &Training institute</p><br></br>
-            <div className="flex justify-between w-full">
-              <span className="text-[#F91B1B] whitespace-nowrap">New York</span>
-              <span className="whitespace-nowrap">Août 2022</span>
-            </div>
-          </div>
-          <div className="flex-1 bg-[#F91B1B70] m-[20px] px-[20px] py-[10px] sm:w-[300px] md:w-[300px] lg:w-[320px] max-w-[320px] hover:cursor-pointer hover:shadow-[#F91B1B] hover:shadow-sm">
-            <div className="flex">
-              <h5 className="text-[18px] text-[#F91B1B]">L'essentiel pour l'Analyste SOC</h5>
-              <img className="w-15 h-15" src={alphorm} alt="alphorm" />
-            </div>
-            <p>Alphorm</p><br></br>
-            <div className="flex justify-between w-full">
-              <span className="text-[#F91B1B] whitespace-nowrap">France</span>
-              <span className="whitespace-nowrap">Décembre 2022</span>
-            </div>
-          </div>
-          </>):(<>
             <div className="flex-1 bg-[#F91B1B70] m-[20px] px-[20px] py-[10px] sm:w-[300px] md:w-[300px] lg:w-[320px] max-w-[320px] hover:cursor-pointer hover:shadow-[#F91B1B] hover:shadow-sm">
-            <div className="flex">
-              <h5 className="text-[18px] text-[#F91B1B] pr-10">ESTI Antanimena</h5>
-              <img className="w-15 h-15" src={esti} alt="esti" />
+              <div className="flex">
+                <h5 className="text-[18px] text-[#F91B1B]">L'essentiel pour l'Analyste SOC</h5>
+                <img className="w-15 h-15" src={alphorm} alt="alphorm" />
+              </div>
+              <p>Alphorm</p><br></br>
+              <div className="flex justify-between w-full">
+                <span className="text-[#F91B1B] whitespace-nowrap">France</span>
+                <span className="whitespace-nowrap">Décembre 2022</span>
+              </div>
             </div>
-            <p>LICENSE en Réseaux et Systèmes</p><br></br>
-            <div className="flex justify-between w-full">
-              <span className="text-[#F91B1B] whitespace-nowrap">Madagascar</span>
-              <span className="whitespace-nowrap">2017 - 2021</span>
+          </>) : (<>
+            <div className="flex-1 bg-[#F91B1B70] m-[20px] px-[20px] py-[10px] sm:w-[300px] md:w-[300px] lg:w-[320px] max-w-[320px] hover:cursor-pointer hover:shadow-[#F91B1B] hover:shadow-sm">
+              <div className="flex">
+                <h5 className="text-[18px] text-[#F91B1B] pr-10">ESTI Antanimena</h5>
+                <img className="w-15 h-15" src={esti} alt="esti" />
+              </div>
+              <p>LICENSE en Réseaux et Systèmes</p><br></br>
+              <div className="flex justify-between w-full">
+                <span className="text-[#F91B1B] whitespace-nowrap">Madagascar</span>
+                <span className="whitespace-nowrap">2017 - 2021</span>
+              </div>
             </div>
-          </div>
           </>)}
         </div>
 
--- src/components/Home/component/Header.jsx
+++ src/components/Home/component/Header.jsx
@@ -4,34 +4,41 @@ function Header() {
   return (
     <>
 
-      <div id="home" className='mb-cover w-full gap-5 px-20 mt-15 p-10'>   
+      <div id="home" className='mb-cover w-full gap-5 px-20 mt-15 p-10'>
 
         <div className='flex flex-col mb-cover container px-4 md:px-15 lg:flex-row items-center w-full'>
 
           <div className="flex-1 mt-20">
             <h2 className='text-white text-5xl'>Je suis</h2>
-            <h1 className='text-red-700 text-7xl font-bold'>Sarobidy <br/> ANDRIAMIALY</h1>
+            <h1 className='text-red-700 text-7xl font-bold'>Sarobidy <br /> ANDRIAMIALY</h1>
             <h2 className='text-white text-4xl'>Consultant en cybersécurité</h2>
             <p className='text-red-200 mb-10-max-w-2xl my-4'>Passionné par le cybersécurité, je conçois et <br />
-            enforces des systèmes d'informations<br/> sûrs et résilients</p>
+              enforces des systèmes d'informations<br /> sûrs et résilients</p>
             <div className="flex-1 transform md:translate-x-30  p-4">
               <h3 className='text-white text-3xl font-bold text-shadow-red-500 text-shadow-lg drop-shadow-2xl drop-shadow-red-500 animate-pulse'> +de 6 ans d'expérience</h3>
             </div>
           </div>
 
           <div className="flex-1">
-            <div className="relative ">
-    
-              <div className="absolute  bg-red-900  w-full h-full rounded-full blur-3xl"></div>
+            <div className="relative w-full h-full flex justify-center items-center py-8">
+              {/* Effet de glow amélioré et élargi */}
+              <div className="absolute bg-red-900/70 w-[340px] h-[340px] rounded-full blur-3xl"></div>
 
-                <div className="relative flex items-center justify-center text-center">
-                <img src={profil} alt="Image" class="rounded-lg mx-auto  object-cover h-[300px] aspect-video w-[300px] z-10" />
+              {/* Container d'image circulaire */}
+              <div className="relative z-10 w-[300px] h-[300px] rounded-full overflow-hidden">
+                <img
+                  src={profil}
+                  alt="Sarobidy ANDRIAMIALY"
+                  className="w-full h-full object-cover object-top" // object-top pour focus sur le visage
+                  style={{ objectPosition: "center 30%" }} // Ajustement fin de la position verticale
+                  loading="eager"
+                />
               </div>
             </div>
           </div>
 
         </div>
-     
+
       </div>
     </>
 
